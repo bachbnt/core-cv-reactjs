54036ea2d4657eda3578acb56aae7b3d224d66ef
bachbnt <51219305+bachbnt@users.noreply.github.com>
2021-04-01 10:56:19 +0700

    change avatar config readme

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -19,8 +19,7 @@ import Skill from '../../components/skill/skill';
 const About = () => {
   const classes = useStyles();
   const item: ProfileItem = {
-    avatar:
-      'https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-9/123094390_1620822461423846_1868681946475942580_o.jpg?_nc_cat=107&ccb=3&_nc_sid=09cbfe&_nc_ohc=ZXHHjGYmh1wAX_bPmPz&_nc_ht=scontent.fsgn5-2.fna&oh=85769d76f8947b7d8726e563b58c2b51&oe=605AF8F8',
+    avatar: 'https://avatars.githubusercontent.com/u/51219305?v=4',
     description:
       'My background is the Master in Engineering Physics. Because of the passion for coding, I decided to chose Software Engineer as my profession. With knowledge and skills of technology, I want to become a Project Manager in the future.',
   };
