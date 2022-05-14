2cb6b3d7f767df39c3bb0e5f370dcb38d5219bda
bachbnt <bachbnt@gmail.com>
2022-05-14 18:47:40 +0700

    fix drawer

--- src/components/drawer/drawer.tsx
+++ src/components/drawer/drawer.tsx
@@ -4,9 +4,13 @@ import {
   ListItem,
   ListItemText,
 } from '@material-ui/core';
+import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { useHistory, useLocation } from 'react-router';
 import clsx from 'clsx';
+import _ from 'lodash';
+import { RootState } from 'src/redux/rootState';
+import { ConfigState } from 'src/redux/config/configState';
 import { routes } from 'src/routes/routes';
 import { Props } from './props';
 import useStyles from './styles';
@@ -18,8 +22,15 @@ const Drawer = (props: Props) => {
   const location = useLocation();
   const { t } = useTranslation();
 
-  const onPageClick = (path: string) => {
-    if (location.pathname !== path) {
+  const config = useSelector<RootState, ConfigState>(
+    (state) => state.configReducer
+  );
+
+  const onPageClick = (name: string, path: string) => {
+    if (
+      (config as any)[`${_.lowerCase(name)}Enable`] &&
+      location.pathname !== path
+    ) {
       history.push(path);
     }
   };
@@ -33,22 +44,26 @@ const Drawer = (props: Props) => {
       anchor='right'
     >
       <List className={clsx(classes.list)}>
-        {routes.map((route) => (
-          <ListItem
-            key={route.name}
-            classes={{
-              root: classes.listItem,
-              selected: classes.selectedListItem,
-            }}
-            button
-            selected={location.pathname === route.path}
-            onClick={() => {
-              onPageClick(route.path);
-            }}
-          >
-            <ListItemText primary={t(route.name)}></ListItemText>
-          </ListItem>
-        ))}
+        {routes.map((route) =>
+          (config as any)?.[`${_.lowerCase(route.name)}Visible`] ? (
+            <ListItem
+              key={route.name}
+              classes={{
+                root: classes.listItem,
+                selected: classes.selectedListItem,
+              }}
+              button
+              selected={location.pathname === route.path}
+              onClick={() => {
+                onPageClick(route.name, route.path);
+              }}
+            >
+              <ListItemText primary={t(route.name)}></ListItemText>
+            </ListItem>
+          ) : (
+            <div key={route.name} />
+          )
+        )}
       </List>
     </MaterialDrawer>
   );
