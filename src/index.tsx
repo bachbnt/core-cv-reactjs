f2a3e4d62f183cfb19db2933e125ecb79e2d217e
bach.bui <bach.bui@mservice.com.vn>
2023-01-10 17:19:41 +0700

    createGenerateClassName

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,9 @@
-import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import {
+  createGenerateClassName,
+  CssBaseline,
+  StylesProvider,
+  ThemeProvider,
+} from '@material-ui/core';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -9,14 +14,20 @@ import styles from './themes/styles';
 import themes from './themes/themes';
 import variables from './themes/variables';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'jss',
+});
+
 render(
-  <ThemeProvider theme={{ ...themes, colors, styles, variables }}>
-    <CssBaseline />
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </ThemeProvider>,
+  <StylesProvider generateClassName={generateClassName}>
+    <ThemeProvider theme={{ ...themes, colors, styles, variables }}>
+      <CssBaseline />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </ThemeProvider>
+  </StylesProvider>,
   document.getElementById('root')
 );
